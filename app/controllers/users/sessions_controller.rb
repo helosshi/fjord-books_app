7aad9c2ecab9ce0c3d7f51295cd62dd96d6bbb67
helosshi <the24sfc@gmail.com>
2021-01-14 02:41:12 +0900

    Revert "i18nの適用まで"
    This reverts commit b3757683e56b5b8f35ff7d0fd7eeb27efb1e1dbd.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,13 +25,13 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  # ログイン後のリダイレクト先
-  def after_sign_in_path_for(*)
+  #ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
     books_path
   end
-
-  # ログアウト後のリダイレクト先
-  def after_sign_out_path_for(*)
+  #ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource)
     new_user_session_path
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,16 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
-  before_action :authenticate_user!, only: %i[mypage edit update]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:mypage, :edit, :update]
   def index
     @users = User.order(:id).page(params[:page]).per(3)
   end
-
   def mypage
     redirect_to user_path(current_user)
   end
-
   def edit
-    redirect_to user_path(@users) unless @users == current_user
+    unless @users == current_user
+      redirect_to user_path(@users)
+    end
   end
 
   def update
@@ -22,16 +20,16 @@ class UsersController < ApplicationController
       redirect_to edit_user_path(current_user)
     end
   end
-
-  def show; end
+  def show
+  end
 
   private
-
   def set_user
     @users = User.find(params[:id])
   end
-
   def user_params
     params.fetch(:user, {}).permit(:address, :email, :post_code, :self_introduction)
   end
+
+
 end
